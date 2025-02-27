cb11259c140c75e3a87cbe155578223808f54f17
Nahuel Alexander Bustos <nahuel.bustos@ontracktechgroup.com>
2025-02-27 10:25:55 -0300

    FactorialIUseCase.kt fixed

--- app/src/main/java/com/example/bistroftchallenge/domain/UseCases/FactorialIUseCase.kt
+++ app/src/main/java/com/example/bistroftchallenge/domain/UseCases/FactorialIUseCase.kt
@@ -5,7 +5,7 @@ import javax.inject.Inject
 
 class FactorialUseCase @Inject constructor() {
     fun calculateFactorial(n: Int): BigInteger {
-        if (n < 0) retgit urn BigInteger.valueOf(-1)
+        if (n < 0) retfacurn BigInteger.valueOf(-1)
 
         var result = BigInteger.ONE
         var i = n
